f55c73a6ad6d50c08934caff0a24977828577c15
Arhelf1 <arhelf1@gmail.com>
2020-11-30 14:56:38 +0300

    Круто, но почему то не работает...

--- test.pl
+++ test.pl
@@ -9,11 +9,30 @@ wlist([]):-!.
 wlist([Text|List]):- write(Text), write(" "), wlist(List).
 
 %Теперь нужна проверка простоты числа
+pr(2):- !.
 pr(X):- 0 =\= X mod 2,
 pr(X,3).
 pr(X,X) :- !. 
-pr(2):- !.
+
 pr(X,Y):- 0=\= X mod Y, Y1 is Y+2, pr(X,Y1).
+
+createlist(X, Y, L):- createlist(X,Y,[], L).
+createlist(X,Y,[Text|List], [Text|List]):- Y>X.
+createlist(X,Y,L1,L):- Nool is X mod Y, Nool ==0, pr(Y), append(L1, [Y], L2), Y1 is Y+1,
+	createlist(X,Y1,L2,L).
+createlist(X,Y,L1,L):- Y1 is Y+1, createlist(X,Y1,L1,L).
+
+checkeleminlist(X,[X|_]:- !.
+checkeleminlist(X,[_|List]):- checkeleminlist(X,List).
+
+unite([], L, L):-!.
+unite([X|List], L, Y):- checkeleminlist(X,L), !, unite(List,L, Y).
+unite([X|List], L, [X|Y]):- unite(X, L, Y).
+
+nonrepeat([X|List], Otvet):- nonrepeat([X|List, [], Otvet).
+nonrepeat([], L, L):-!.
+nonrepeat([X,List], Otvet1, L):- createlist(X,2, Otvet), unite(Otvet1,Otvet,Otvet2), 
+	checkelemlist(List,Otvet2, L), !.
 
 main:-
 	write("List length: "),read(N),
